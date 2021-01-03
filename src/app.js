e3d9e2f6d9baa8f2888e6dfd35f6e9b427fc03ad
Pawel <74410157+pawelkudzia@users.noreply.github.com>
2021-01-03 14:14:53 +0100

    Add basic app scaffolding and test endpoint
    It adds /api/v1/test endpoint which is used for checking if basic app works.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
-// TODO
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import testRouter from './routes/testRoutes.js';
+
+
+// app init
+dotenv.config({ path: './config.env' });
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+// database
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to database!'));
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+// api
+app.use('/api/v1/test', testRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can not find ${req.originalUrl} on this server!`
+    });
+});
+
+
+// server
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}.`));
--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -1,1 +1,24 @@
-// TODO
+import express from 'express';
+
+// endpoint handlers
+const apiTest = async (req, res) => {
+    try {
+        res.status(200).json({
+            status: 'success',
+            message: `API is working: ${req.originalUrl}.`
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Something went wrong.'
+        });
+    }
+};
+
+// router
+const router = express.Router();
+
+// endpoints
+router.route('/').get(apiTest);
+
+export default router;
