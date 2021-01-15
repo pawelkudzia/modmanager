1056062ce73ce98a013a23fc37fce4d3024d0821
Pawel <74410157+pawelkudzia@users.noreply.github.com>
2021-01-15 22:22:58 +0100

    Update game and mod components
    It adds form in mod component and fix error message in game component.

--- ModManagerClient/src/app/games/games.component.ts
+++ ModManagerClient/src/app/games/games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-games',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+  submitted: boolean = false;
+  addGameForm: FormGroup;
 
-  constructor() { }
+  genres: string[] = [
+    'Action', 'Adventure', 'Role-playing', 'Shooter', 'Simulation', 'Sports', 'Strategy', 'Misc'
+  ];
+
+  constructor(private _fb: FormBuilder) { }
+
+  get name(): AbstractControl {
+    return this.addGameForm.get('name');
+  }
+
+  get description(): AbstractControl {
+    return this.addGameForm.get('description');
+  }
+
+  get genre(): AbstractControl {
+    return this.addGameForm.get('genre');
+  }
+
+  get developer(): AbstractControl {
+    return this.addGameForm.get('developer');
+  }
+
+  get engine(): AbstractControl {
+    return this.addGameForm.get('engine');
+  }
+
+  get platforms(): AbstractControl {
+    return this.addGameForm.get('platforms');
+  }
 
   ngOnInit(): void {
+    this.addGameForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(1000)]],
+      genre: [this.genres[0], Validators.required],
+      developer: ['', [Validators.required, Validators.minLength(3)]],
+      engine: ['', [Validators.required, Validators.minLength(3)]],
+      platforms: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('data was submitted!');
+    console.log(this.addGameForm);
+  }
+
+  newForm() {
+    this.submitted = false;
+    this.addGameForm.reset();
   }
 
 }
--- ModManagerClient/src/app/mods/mods.component.ts
+++ ModManagerClient/src/app/mods/mods.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mods',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mods.component.css']
 })
 export class ModsComponent implements OnInit {
+  submitted: boolean = false;
+  addModForm: FormGroup;
 
-  constructor() { }
+  games: string[] = [
+    'Game 1', 'Game 2'
+  ];
+
+  constructor(private _fb: FormBuilder) { }
+
+  get name(): AbstractControl {
+    return this.addModForm.get('name');
+  }
+
+  get description(): AbstractControl {
+    return this.addModForm.get('description');
+  }
+
+  get game(): AbstractControl {
+    return this.addModForm.get('game');
+  }
+
+  get author(): AbstractControl {
+    return this.addModForm.get('author');
+  }
 
   ngOnInit(): void {
+    this.addModForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(1000)]],
+      game: [this.games[0], Validators.required],
+      author: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('data was submitted!');
+    console.log(this.addModForm);
+  }
+
+  newForm() {
+    this.submitted = false;
+    this.addModForm.reset();
   }
 
 }
