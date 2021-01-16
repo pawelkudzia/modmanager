a3a52bae82d5111811c9a2642f15c11d13d5650d
Pawel <74410157+pawelkudzia@users.noreply.github.com>
2021-01-16 22:01:01 +0100

    Disable authorization middleware for Game
    For testing client appliation the middleware was disabled.

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -7,12 +7,12 @@ const router = express.Router();
 
 // endpoints
 router.route('/')
-    .get(authController.protect, gameController.getAllGames)
-    .post(authController.protect, authController.restrictTo('admin'), gameController.createGame);
+    .get(/*authController.protect,*/ gameController.getAllGames)
+    .post(/*authController.protect, authController.restrictTo('admin'),*/ gameController.createGame);
 
 router.route('/:id')
-    .get(authController.protect, gameController.getGame)
-    .patch(authController.protect, authController.restrictTo('admin'), gameController.updateGame)
-    .delete(authController.protect, authController.restrictTo('admin'), gameController.deleteGame);
+    .get(/*authController.protect,*/ gameController.getGame)
+    .patch(/*authController.protect, authController.restrictTo('admin'),*/ gameController.updateGame)
+    .delete(/*authController.protect, authController.restrictTo('admin'),*/ gameController.deleteGame);
 
 export default router;
