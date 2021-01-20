73e0e2d4f5d84b3e5ddb65d4e6856e3082b63992
Pawel <74410157+pawelkudzia@users.noreply.github.com>
2021-01-20 11:24:24 +0100

    Display name of game in single Mod view
    Name of game is displayed and link navigates to single Game view.

--- ModManagerClient/src/app/mod/mod.component.ts
+++ ModManagerClient/src/app/mod/mod.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Game } from '../contracts/game';
 import { Mod } from '../contracts/mod';
+import { GameService } from '../services/game.service';
 import { ModService } from '../services/mod.service';
 
 @Component({
@@ -13,11 +15,13 @@ export class ModComponent implements OnInit {
   response = null;
   error = null;
   mod: Mod;
+  game: Game;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _modService: ModService
+    private _modService: ModService,
+    private _gameService: GameService
   ) { }
 
   ngOnInit(): void {
@@ -27,6 +31,17 @@ export class ModComponent implements OnInit {
       response => {
         this.response = response;
         this.mod = this.response.data.mod;
+
+        this._gameService.getGame(this.mod.game).subscribe(
+          response => {
+            this.response = response;
+            this.game = this.response.data.game;
+          },
+          error => {
+            this.error = error;
+            this._router.navigate(['/404']);
+          }
+        );
       },
       error => {
         this.error = error;
